5ba753db8c1d2697f9c71d4a9487e9af302aa0e8
Julia <blindemons@gmail.com>
2020-11-11 10:37:20 +0300

    comments added

--- src/main/java/com/epam/jwd/core_final/context/menu/MenuContext.java
+++ src/main/java/com/epam/jwd/core_final/context/menu/MenuContext.java
@@ -1,6 +1,5 @@
 package com.epam.jwd.core_final.context.menu;
 
-import com.epam.jwd.core_final.context.strategy.CrewStrategy;
 import com.epam.jwd.core_final.domain.CrewMember;
 import com.epam.jwd.core_final.domain.FlightMission;
 import com.epam.jwd.core_final.domain.Spaceship;
@@ -69,122 +68,132 @@ public class MenuContext {
     }
 
     private void assignStartMenuOptions() {
-        this.storage.put(new KeyMenu(Menu.START, 1), () -> Menu.MISSION_CONTROL); // go to mission control from start menu
+        this.storage.put(new KeyMenu(Menu.START, 1), () -> Menu.MISSION_CONTROL);   /* go to mission control from start menu*/
 
-        this.storage.put(new KeyMenu(Menu.START, 2), () -> Menu.CREW_CONTROL); // go to crew control menu from start menu
+        this.storage.put(new KeyMenu(Menu.START, 2), () -> Menu.CREW_CONTROL); /* go to crew control menu from start menu*/
 
-        this.storage.put(new KeyMenu(Menu.START, 3), () -> Menu.SPACESHIP_CONTROL); // go to spaceship control menu from start menu
+        this.storage.put(new KeyMenu(Menu.START, 3), () -> Menu.SPACESHIP_CONTROL);   /* go to spaceship control menu from start menu*/
 
-        this.storage.put(new KeyMenu(Menu.START, 4), () -> Menu.EXIT); // exit from start menu
+        this.storage.put(new KeyMenu(Menu.START, 4), () -> Menu.EXIT); /* exit from start menu*/
     }
 
     private void assignMissionControlMenuOptions() {
+        /*create a mission*/
         this.storage.put(new KeyMenu(Menu.MISSION_CONTROL, 1), () -> {
             MenuMissionService service = MenuMissionServiceImpl.getInstance();
             service.createMission();
             return Menu.MISSION_CONTROL;
-        }); // create a mission
+        });
 
-        this.storage.put(new KeyMenu(Menu.MISSION_CONTROL, 2), () -> Menu.MISSION_CRITERIA); // go to mission criteria menu
+        this.storage.put(new KeyMenu(Menu.MISSION_CONTROL, 2), () -> Menu.MISSION_CRITERIA); /*go to mission criteria menu*/
 
-        this.storage.put(new KeyMenu(Menu.MISSION_CONTROL, 3), () -> Menu.START); // exit from mission control menu
+        this.storage.put(new KeyMenu(Menu.MISSION_CONTROL, 3), () -> Menu.START);  /*exit from mission control menu*/
     }
 
     private void assignMissionCriteriaOptions() {
-
+        /*search all missions*/
         this.storage.put(new KeyMenu(Menu.MISSION_CRITERIA, 1), () -> {
             MissionService service = MissionServiceImpl.getInstance();
-
             for (FlightMission mission : service.findAllMissions()) {
-                MissionServiceImpl.getInstance().writeToJson(mission);
-                System.out.println(mission.toString());
+                if (mission != null) {
+                    MissionServiceImpl.getInstance().writeToJson(mission);
+                    System.out.println(mission.toString());
+                }
             }
             return Menu.MISSION_CRITERIA;
-        }); //search all missions
+        });
 
+        /*search one mission by id*/
         this.storage.put(new KeyMenu(Menu.MISSION_CRITERIA, 2), () -> {
             MenuMissionService service = MenuMissionServiceImpl.getInstance();
             try {
                 service.searchMissionById();
             } catch (StorageException e) {
                 System.out.println(e.getMessage());
-                logger.debug(e.getMessage());
+                logger.warn(e.getMessage());
                 return Menu.MISSION_CRITERIA;
             }
             return Menu.MISSION_UPDATE;
-        }); // search one mission by id
+        });
 
+        /*search one mission by name*/
         this.storage.put(new KeyMenu(Menu.MISSION_CRITERIA, 3), () -> {
             MenuMissionService service = MenuMissionServiceImpl.getInstance();
             try {
                 service.searchMissionByName();
             } catch (StorageException e) {
                 System.out.println(e.getMessage());
-                logger.debug(e.getMessage());
+                logger.warn(e.getMessage());
                 return Menu.MISSION_CRITERIA;
             }
             return Menu.MISSION_UPDATE;
-        }); // search one mission by name
+        });
 
+        /*search one mission by distance*/
         this.storage.put(new KeyMenu(Menu.MISSION_CRITERIA, 4), () -> {
             MenuMissionService service = MenuMissionServiceImpl.getInstance();
             try {
                 service.searchMissionByDistance();
             } catch (StorageException e) {
                 System.out.println(e.getMessage());
-                logger.debug(e.getMessage());
+                logger.warn(e.getMessage());
                 return Menu.MISSION_CRITERIA;
             }
             return Menu.MISSION_UPDATE;
-        }); // search one mission by distance
+        });
 
+        /*search all missions by start date*/
         this.storage.put(new KeyMenu(Menu.MISSION_CRITERIA, 5), () -> {
             MenuMissionService service = MenuMissionServiceImpl.getInstance();
             try {
                 service.searchAllByStartDate();
             } catch (StorageException e) {
                 System.out.println(e.getMessage());
-                logger.debug(e.getMessage());
+                logger.warn(e.getMessage());
             }
             return Menu.MISSION_CRITERIA;
-        }); // search all missions by start date
+        });
 
-        this.storage.put(new KeyMenu(Menu.MISSION_CRITERIA, 6), () -> Menu.MISSION_CONTROL); // exit from mission criteria menu
+        this.storage.put(new KeyMenu(Menu.MISSION_CRITERIA, 6), () -> Menu.MISSION_CONTROL); /*exit from mission criteria menu*/
 
     }
 
     private void assignMissionUpdateOptions() {
+        /*update mission's name*/
         this.storage.put(new KeyMenu(Menu.MISSION_UPDATE, 1), () -> {
             MenuMissionService service = MenuMissionServiceImpl.getInstance();
             service.updateMissionName();
 
             return Menu.MISSION_CONTROL;
-        }); // update mission name
+        });
 
+        /*update mission distance*/
         this.storage.put(new KeyMenu(Menu.MISSION_UPDATE, 2), () -> {
             MenuMissionService service = MenuMissionServiceImpl.getInstance();
             service.updateMissionDistance();
 
             return Menu.MISSION_CONTROL;
-        }); // update mission distance
+        });
 
+        /*update mission duration*/
         this.storage.put(new KeyMenu(Menu.MISSION_UPDATE, 3), () -> {
             MenuMissionService service = MenuMissionServiceImpl.getInstance();
             service.updateMissionDuration();
 
             return Menu.MISSION_CONTROL;
-        }); //update mission duration
+        });
 
-        this.storage.put(new KeyMenu(Menu.MISSION_UPDATE, 4), () -> Menu.MISSION_CONTROL); // exit from mission update
+        this.storage.put(new KeyMenu(Menu.MISSION_UPDATE, 4), () -> Menu.MISSION_CONTROL);    /*exit from mission update*/
     }
 
     private void assignCrewControlMenuOptions() {
-        this.storage.put(new KeyMenu(Menu.CREW_CONTROL, 1), () -> Menu.CREW_CRITERIA); // go to crew criteria
+        this.storage.put(new KeyMenu(Menu.CREW_CONTROL, 1), () -> Menu.CREW_CRITERIA); /*go to crew criteria*/
 
-        this.storage.put(new KeyMenu(Menu.CREW_CONTROL, 2), () -> Menu.START); // go to start menu
+        this.storage.put(new KeyMenu(Menu.CREW_CONTROL, 2), () -> Menu.START); /*go to start menu*/
     }
 
     private void assignCrewCriteriaOptions() {
+        /*search all crew members*/
         this.storage.put(new KeyMenu(Menu.CREW_CRITERIA, 1), () -> {
             CrewService service = CrewServiceImpl.getInstance();
 
@@ -193,90 +202,98 @@ public class MenuContext {
             }
 
             return Menu.CREW_CRITERIA;
-        }); // search all crew members
+        });
 
+        /*search one crew member by id*/
         this.storage.put(new KeyMenu(Menu.CREW_CRITERIA, 2), () -> {
             MenuCrewService service = MenuCrewServiceImpl.getInstance();
             try {
                 service.searchCrewMemberById();
             } catch (StorageException e) {
                 System.out.println(e.getMessage());
-                logger.debug(e.getMessage());
+                logger.warn(e.getMessage());
                 return Menu.CREW_CRITERIA;
             }
             return Menu.CREW_UPDATE;
-        }); // search one crew member by id
+        });
 
+        /*search one crew member by name*/
         this.storage.put(new KeyMenu(Menu.CREW_CRITERIA, 3), () -> {
             MenuCrewService service = MenuCrewServiceImpl.getInstance();
             try {
                 service.searchCrewMemberByName();
             } catch (StorageException e) {
                 System.out.println(e.getMessage());
-                logger.debug(e.getMessage());
+                logger.warn(e.getMessage());
                 return Menu.CREW_CRITERIA;
             }
             return Menu.CREW_UPDATE;
-        }); // search one crew member by name
+        });
 
+        /*search all crew members by rank*/
         this.storage.put(new KeyMenu(Menu.CREW_CRITERIA, 4), () -> {
             MenuCrewService service = MenuCrewServiceImpl.getInstance();
             try {
                 service.searchAllCrewMembersByRank();
             } catch (StorageException e) {
                 System.out.println(e.getMessage());
-                logger.debug(e.getMessage());
+                logger.warn(e.getMessage());
                 return Menu.CREW_CRITERIA;
             }
             return Menu.CREW_CRITERIA;
-        }); // search all crew members by rank
+        });
 
+        /*search all crew members by role*/
         this.storage.put(new KeyMenu(Menu.CREW_CRITERIA, 5), () -> {
             MenuCrewService service = MenuCrewServiceImpl.getInstance();
             try {
                 service.searchAllCrewMembersByRole();
             } catch (StorageException e) {
                 System.out.println(e.getMessage());
-                logger.debug(e.getMessage());
+                logger.warn(e.getMessage());
             }
             return Menu.CREW_CRITERIA;
-        }); // search all crew members by role
+        });
 
-        this.storage.put(new KeyMenu(Menu.CREW_CRITERIA, 6), () -> Menu.CREW_CONTROL); // go to crew control panel
+        this.storage.put(new KeyMenu(Menu.CREW_CRITERIA, 6), () -> Menu.CREW_CONTROL); /*go to crew control panel*/
     }
 
     private void assignCrewUpdateOptions() {
+        /*update crew member's name*/
         this.storage.put(new KeyMenu(Menu.CREW_UPDATE, 1), () -> {
             MenuCrewService service = MenuCrewServiceImpl.getInstance();
             service.updateCrewMembersName();
 
             return Menu.CREW_CONTROL;
-        }); // update crew member's name
+        });
 
+        /*update crew member's role*/
         this.storage.put(new KeyMenu(Menu.CREW_UPDATE, 2), () -> {
             MenuCrewService service = MenuCrewServiceImpl.getInstance();
             service.updateCrewMembersRole();
 
             return Menu.CREW_CONTROL;
-        }); // update crew member's role
+        });
 
+        /*update crew member's rank*/
         this.storage.put(new KeyMenu(Menu.CREW_UPDATE, 3), () -> {
             MenuCrewService service = MenuCrewServiceImpl.getInstance();
             service.updateCrewMembersRank();
 
             return Menu.CREW_CONTROL;
-        }); // update crew member's rank
+        });
 
-        this.storage.put(new KeyMenu(Menu.CREW_UPDATE, 4), () -> Menu.CREW_CONTROL); // go to crew control panel
+        this.storage.put(new KeyMenu(Menu.CREW_UPDATE, 4), () -> Menu.CREW_CONTROL); /*go to crew control panel*/
     }
 
     private void assignSpaceshipControlMenuOptions() {
-        this.storage.put(new KeyMenu(Menu.SPACESHIP_CONTROL, 1), () -> Menu.SPACESHIP_CRITERIA); // go to  spaceship criteria
+        this.storage.put(new KeyMenu(Menu.SPACESHIP_CONTROL, 1), () -> Menu.SPACESHIP_CRITERIA); /*go to  spaceship criteria*/
 
-        this.storage.put(new KeyMenu(Menu.SPACESHIP_CONTROL, 2), () -> Menu.START); // go to start menu
+        this.storage.put(new KeyMenu(Menu.SPACESHIP_CONTROL, 2), () -> Menu.START); /*go to start menu*/
     }
 
     private void assignSpaceshipCriteriaOptions() {
+        /*search all spaceships*/
         this.storage.put(new KeyMenu(Menu.SPACESHIP_CRITERIA, 1), () -> {
             SpaceshipService service = SpaceshipServiceImpl.getInstance();
 
@@ -285,62 +302,67 @@ public class MenuContext {
                 System.out.println(spaceship.toString());
             }
             return Menu.SPACESHIP_CRITERIA;
-        }); //search all spaceships
+        });
 
         this.storage.put(new KeyMenu(Menu.SPACESHIP_CRITERIA, 2), () -> {
+            /*search one by id*/
             MenuSpaceshipService service = MenuSpaceshipServiceImpl.getInstance();
             try {
                 service.searchSpaceshipById();
             } catch (StorageException e) {
                 System.out.println(e.getMessage());
-                logger.debug(e.getMessage());
+                logger.warn(e.getMessage());
                 return Menu.SPACESHIP_CRITERIA;
             }
             return Menu.SPACESHIP_UPDATE;
-        }); // search one by id
+        });
 
+        /*search one spaceship by name*/
         this.storage.put(new KeyMenu(Menu.SPACESHIP_CRITERIA, 3), () -> {
             MenuSpaceshipService service = MenuSpaceshipServiceImpl.getInstance();
             try {
                 service.searchSpaceshipByName();
             } catch (StorageException e) {
                 System.out.println(e.getMessage());
-                logger.debug(e.getMessage());
+                logger.warn(e.getMessage());
                 return Menu.SPACESHIP_CRITERIA;
             }
             return Menu.SPACESHIP_UPDATE;
-        }); // search one spaceship by name
+        });
 
+        /*search one by distance*/
         this.storage.put(new KeyMenu(Menu.SPACESHIP_CRITERIA, 4), () -> {
             MenuSpaceshipService service = MenuSpaceshipServiceImpl.getInstance();
             try {
                 service.searchSpaceshipByDistance();
             } catch (StorageException e) {
                 System.out.println(e.getMessage());
-                logger.debug(e.getMessage());
+                logger.warn(e.getMessage());
                 return Menu.SPACESHIP_CRITERIA;
             }
             return Menu.SPACESHIP_UPDATE;
-        }); // search one by distance
+        });
 
-        this.storage.put(new KeyMenu(Menu.SPACESHIP_CRITERIA, 5), () -> Menu.SPACESHIP_CONTROL); // go to spaceship control menu
+        this.storage.put(new KeyMenu(Menu.SPACESHIP_CRITERIA, 5), () -> Menu.SPACESHIP_CONTROL); /*go to spaceship control menu*/
     }
 
     private void assignSpaceshipUpdateOptions() {
+        /*update spaceship's name*/
         this.storage.put(new KeyMenu(Menu.SPACESHIP_UPDATE, 1), () -> {
             MenuSpaceshipService service = MenuSpaceshipServiceImpl.getInstance();
             service.updateSpaceshipsName();
 
             return Menu.SPACESHIP_CONTROL;
-        }); // update spaceship's name
+        });
 
+        /*update spaceship's distance*/
         this.storage.put(new KeyMenu(Menu.SPACESHIP_UPDATE, 2), () -> {
             MenuSpaceshipService service = MenuSpaceshipServiceImpl.getInstance();
             service.updateSpaceshipsDistance();
 
             return Menu.SPACESHIP_CONTROL;
-        }); // update spaceship's distance
+        });
 
-        this.storage.put(new KeyMenu(Menu.SPACESHIP_UPDATE, 3), () -> Menu.SPACESHIP_CONTROL); // go to spaceship control menu
+        this.storage.put(new KeyMenu(Menu.SPACESHIP_UPDATE, 3), () -> Menu.SPACESHIP_CONTROL);   /*go to spaceship control menu*/
     }
 }
